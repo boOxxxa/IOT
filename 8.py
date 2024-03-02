c23bbfcd72ea1090e3866abe5ded2d4df9e6e9c1
vadimpechenin <vadim.pechenin2015@gmail.com>
2024-03-02 13:29:09 +0400

    Practise2

---
+++ 8.py
@@ -0,0 +1,19 @@
+import numpy as np
+r = 2.5
+v = 1
+vecs = np.array(
+ [
+ [1.0, 0.0, 2.0],
+ [-1.0, 0.5, 2.0],
+ [-2.0, 1.5, 0.0],
+ [2.5, -1.2, -0.5],
+ [1.5, 0.2, -0.2]
+ ]
+)
+def closest(vecs,v, r):
+    for i in range(len(vecs)):
+        for j in range(len(vecs[i])):
+
+print(closest(vecs, v, r)) # Вернет 2
+r = 0.1
+v = 0
